3d84dcf76e97c2d14f94d024817b3ff21b007f68
Andrew Male <amale@zibings.com>
2018-09-12 01:01:02 -0400

    Working on Logger class

--- Log/Logger.php
+++ Log/Logger.php
@@ -3,8 +3,60 @@
 	namespace Stoic\Log;
 
 	use Psr\Log\AbstractLogger;
-	use Psr\Log\LogLevel;
 
 	class Logger extends AbstractLogger {
-		
+		/**
+		 * Holds all assigned appenders.
+		 * 
+		 * @var LogAppenderBase[]
+		 */
+		private $appenders = array();
+		/**
+		 * Holds all messages for appenders.
+		 * 
+		 * @var LogMessage[]
+		 */
+		private $messages = array();
+
+
+		/**
+		 * Instantiates a new Logger object.
+		 * 
+		 * Creates a new Logger object.  Optionally
+		 * accepts an array of LogAppenderBase objects
+		 * to assign to appender stack.  Any objects in
+		 * array that don't implement LogAppenderBase
+		 * are simply ignored.
+		 * 
+		 * @param null|LogAppenderBase[] $appenders Optional collection of LogAppenderBase objects to assign.
+		 */
+		public function __construct(array $appenders = null) {
+			if ($appenders !== null && count($appenders) > 0) {
+				foreach (array_values($appenders) as $appdr) {
+					if ($appdr instanceof LogAppenderBase) {
+						$this->appenders[] = $appdr;
+					}
+				}
+			}
+
+			return;
+		}
+
+		/**
+		 * Logs with an arbitrary level.
+		 * 
+		 * Generates a Stoic\LogMessage object for
+		 * the provided message
+		 *
+		 * @param string $level
+		 * @param string $message
+		 * @param array $context
+		 */
+		public function log($level, $message, array $context = array()) {
+
+		}
+
+		protected function interpolate($message, array $context) {
+			
+		}
 	}
