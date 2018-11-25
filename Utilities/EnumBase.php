40571488e264552f15802ae9233486916a86fdc0
Andrew Male <amale@zibings.com>
2018-11-24 23:18:23 -0500

    Adding code-docs for Enum abstract class

--- Utilities/EnumBase.php
+++ Utilities/EnumBase.php
@@ -2,15 +2,51 @@
 
 	namespace Stoic\Utilities;
 
+	/**
+	 * Abstract class to provide basic Enum type
+	 * functionality.
+	 *
+	 * @package Stoic\Utilities
+	 * @version 1.0.0
+	 */
 	abstract class EnumBase implements \JsonSerializable {
+		/**
+		 * Static internal cache of const lookups.
+		 *
+		 * @var array
+		 */
 		private static $constCache = [];
 		
-		
+
+		/**
+		 * Internal storage for name.
+		 *
+		 * @var null|string
+		 */
 		private $name = null;
+		/**
+		 * Internal storage for value.
+		 *
+		 * @var null|integer
+		 */
 		private $value = null;
+		/**
+		 * Determines whether or not to serialize as the
+		 * name of the set value.
+		 *
+		 * @var boolean
+		 */
 		private $serializeAsName = true;
 
 
+		/**
+		 * Static method to return a new Enum object using
+		 * the name instead of the value for initialization.
+		 *
+		 * @param string $string String to use as name.
+		 * @param boolean $serializeAsName Causes object to serialize into the name of the set value, defaults to true.
+		 * @return object
+		 */
 		public static function fromString($string, $serializeAsName = true) {
 			$class = get_called_class();
 
@@ -26,6 +62,12 @@
 			return $ret;
 		}
 
+		/**
+		 * Static method to return the const lookup for the
+		 * called class.
+		 *
+		 * @return array
+		 */
 		public static function getConstList() {
 			$cclass = get_called_class();
 
@@ -46,6 +88,13 @@
 			return static::$constCache[$cclass];
 		}
 
+		/**
+		 * Static method to validate a name against the Enum
+		 * object's possible constants.
+		 *
+		 * @param string $name String to use as name.
+		 * @return boolean
+		 */
 		public static function validName($name) {
 			$consts = static::getConstList();
 			$lowered = \mb_strtolower($name);
@@ -53,6 +102,13 @@
 			return array_key_exists($lowered, $consts['name']) !== false;
 		}
 
+		/**
+		 * Static method to validate a value against the Enum
+		 * object's possible constants.
+		 *
+		 * @param integer $value Integer to use as value.
+		 * @return boolean
+		 */
 		public static function validValue($value) {
 			$consts = static::getConstList();
 
@@ -60,6 +116,12 @@
 		}
 
 
+		/**
+		 * Instantiates a new Enum object.
+		 *
+		 * @param null|integer $value Integer to use as value, defaults to null.
+		 * @param mixed $serializeAsName Causes object to serialize into the name of the set value, defaults to true.
+		 */
 		public function __construct($value = null, $serializeAsName = true) {
 			$this->serializeAsName = $serializeAsName;
 
@@ -73,18 +135,46 @@
 			return;
 		}
 
+		/**
+		 * Serializes Enum object to its string representation.
+		 *
+		 * @return string
+		 */
 		public function __toString() {
-			return $this->name ?? '';
+			if ($this->serializeAsName) {
+				return $this->name ?? '';
+			}
+
+			return "{$this->value}" ?? '';
 		}
 
+		/**
+		 * Retrieves the string name of the currently
+		 * set value.
+		 *
+		 * @return null|string
+		 */
 		public function getName() {
 			return $this->name;
 		}
 
+		/**
+		 * Retrieves the integer representation of the
+		 * currently set value.
+		 *
+		 * @return null|integer
+		 */
 		public function getValue() {
 			return $this->value;
 		}
 
+		/**
+		 * Determines if the current set value is the same
+		 * as the given value.
+		 *
+		 * @param integer $value Integer to test against current value.
+		 * @return boolean
+		 */
 		public function is($value) {
 			if ($this->value === null || $this->value !== $value) {
 				return false;
@@ -93,11 +183,12 @@
 			return true;
 		}
 
+		/**
+		 * Serializes Enum object to its string representation.
+		 *
+		 * @return string
+		 */
 		public function jsonSerialize() {
-			if ($this->serializeAsName) {
-				return $this->name ?? '';
-			}
-
-			return "{$this->value}" ?? '';
+			return $this->__toString();
 		}
 	}
